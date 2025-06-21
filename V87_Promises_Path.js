c7fae816334415826310a17870d1c2c50d435bce
Manojreddy0509 <161306313+Manojreddy0509@users.noreply.github.com>
2025-06-21 22:39:22 +0530

    Update V87_Promises_Path.js

--- V87_Promises_Path.js
+++ V87_Promises_Path.js
@@ -12,6 +12,6 @@ let myPath = "C:\\Users\\Users\\Manoj\\Downloads\\Sigma web dev\\harry.txt"
 console.log(path.extname(myPath))
 
 console.log(path.dirname(myPath))
-console.log(path.basename(myPath))
+//console.log(path.basename(myPath))
 
 console.log(path.join("c:/", "programs\\harry.txt"))
